56dd2ad15df6712011033255cc0035ff260fb185
Anorld Mukone <manorldsapiens@gmail.com>
2018-12-19 14:43:13 +0300

    add incident model

---
+++ app/models/incident.py
@@ -0,0 +1,15 @@
+# app/models/redflag.py
+from datetime import datetime, timedelta
+
+class Incident:
+
+	def __init__(self, *args):
+		self.id = uuid
+		self.createdOn = datetime.now()
+		self.createdBy = CreatedBy
+		self.doc_type = doc_type
+        self.location = location
+		self.status = status
+		self.images = images
+        self.videos = videos
+        self.comment = comment
