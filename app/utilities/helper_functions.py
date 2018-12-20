c12876b5a05e0b59e3f1c0383e38e54ac67bb95a
Anorld Mukone <manorldsapiens@gmail.com>
2018-12-20 05:48:57 +0300

    add functional routes

--- app/utilities/helper_functions.py
+++ app/utilities/helper_functions.py
@@ -1,8 +1,14 @@
 from app.models.incident import Incident
 from app.data_store.data import incidents
+from flask import make_response, jsonify
 
 
 class Helper_Functions:
+
+    @staticmethod
+    def the_return_method(status, data, message):
+        return make_response(jsonify({"status": status, "data": data, "message": message})), 201
+
 
     @staticmethod
     def get_red_flags():
@@ -16,3 +22,41 @@ class Helper_Functions:
         for incident in incidents:
             if incident.id == int(id):
                 return incident.to_json_object()
+    
+    @staticmethod
+    def get_dict_data_from_list(list_data):
+        return {
+         			"created_by": list_data[0],
+         			"doc_type": list_data[1],
+         			"location": list_data[2],
+         			"status": list_data[3],
+         			"images": list_data[4],
+         			"videos": list_data[5],
+         			"comment": list_data[6]
+        }
+
+    @staticmethod
+    def validate_incident_input(data):
+        return True
+
+    @staticmethod
+    def update_location(id, location):
+        for incident in incidents:
+            if incident.id == int(id):
+                incident.location = location
+                return incident.to_json_object()
+
+    @staticmethod
+    def update_comment(id, comment):
+        for incident in incidents:
+            if incident.id == int(id):
+                incident.comment = comment
+                return incident.to_json_object()
+
+    @staticmethod
+    def delete_redflag(id):
+        for incident in incidents:
+            if incident.id == int(id):
+                incidents.remove(incident)
+                return True
+        return False
--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ import re
 from flask import Flask, request, jsonify, make_response, json
 from datetime import datetime
 from instance.config import app_config
+from app.data_store.data import incidents
 
 
 def create_app(config_name):
@@ -31,19 +32,38 @@ def create_app(config_name):
 
     @app.route('/api/v1/red-flags', methods=['POST'])
     def create_redflag():
-        pass
+        created_by = json.loads(request.data)['created_by']
+        doc_type = json.loads(request.data)['doc_type']
+        location = json.loads(request.data)['location']
+        status = json.loads(request.data)['status']
+        images = json.loads(request.data)['images']
+        videos = json.loads(request.data)['videos']
+        comment = json.loads(request.data)['comment']
+
+        data = Helper_Functions.get_dict_data_from_list([created_by, doc_type, location,
+                status, images, videos, comment])
+        if Helper_Functions.validate_incident_input(data):
+            red_flag = Incident(data)
+            incidents.append(red_flag)
+            return Helper_Functions.the_return_method(201, red_flag.to_json_object(), "Created red-flag record")
     
     @app.route('/api/v1/red-flags/<red_flag_id>/location', methods=['PATCH'])
     def update_redflag_location(red_flag_id):
-        pass
+        location = json.loads(request.data)['location']
+        data = Helper_Functions.update_location(red_flag_id, location)
+        return Helper_Functions.the_return_method(200, data, "Updated red-flag record’s location")
 
     @app.route('/api/v1/red-flags/<red_flag_id>/comment', methods=['PATCH'])
     def update_redflag_comment(red_flag_id):
-        pass
+        comment = json.loads(request.data)['comment']
+        data = Helper_Functions.update_comment(red_flag_id, comment)
+        return Helper_Functions.the_return_method(200, data, "Updated red-flag record’s comment")
 
     @app.route('/api/v1/red-flags/<red_flag_id>', methods=['DELETE'])
     def delete_redflag(red_flag_id):
-        pass
+        if Helper_Functions.delete_redflag(red_flag_id):
+            data = ""
+            return Helper_Functions.the_return_method(200, data, "red-flag record has been deleted”")
 
     from .auth import auth_blueprint
     app.register_blueprint(auth_blueprint)
