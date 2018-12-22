07245187f53e015311a5a1075211c743102cfe0b
Anorld Mukone <manorldsapiens@gmail.com>
2018-12-22 03:45:10 +0300

    add register validation

--- app/auth/views.py
+++ app/auth/views.py
@@ -9,6 +9,7 @@ from app.models.user import User
 from app.data_store.data import users
 from app.utilities.helper_functions import Helper_Functions
 from app.utilities.login_validation import Login_Validation
+from app.utilities.register_validation import Register_Validation
 
 class RegistrationView(MethodView):
     """This class registers a new user."""
@@ -22,15 +23,21 @@ class RegistrationView(MethodView):
         phonenumber = json.loads(request.data)['phonenumber']
         username = json.loads(request.data)['username']
 
-        if Helper_Functions.email_exists_already(email) is True:
-            return make_response(jsonify({"status": 400, "message": "That Email already is registered. Login or use a different Email to register."})), 400
+        validate_input = Register_Validation(
+            {"firstname": firstname, "lastname": lastname, "othernames": othernames, "email": email, "password": password, "phonenumber": phonenumber, "username": username})
+        validated_input = validate_input.check_input()
+        if validated_input[0] == 200:
+            if Helper_Functions.email_exists_already(email) is True:
+                return make_response(jsonify({"status": 400, "message": "That Email already is registered. Login or use a different Email to register."})), 400
+            else:
+                new_user_info_list = [ firstname, lastname, othernames, email, password, phonenumber, username]
+                new_user = User(Helper_Functions.get_dict_data_from_list_user(new_user_info_list))
+                
+                users.append(new_user)
+                return make_response(jsonify({"status":201, "data":new_user.to_json_object(), "message":"You registered successfully. Login to continue."})), 201
         else:
-            new_user_info_list = [ firstname, lastname, othernames, email, password, phonenumber, username]
-            new_user = User(Helper_Functions.get_dict_data_from_list_user(new_user_info_list))
-            
-            users.append(new_user)
-            return make_response(jsonify({"status":201, "data":new_user.to_json_object(), "message":"You registered successfully. Login to continue."})), 201
-            
+            return Helper_Functions.the_return_method(validated_input[0], None, validated_input[1])
+
 
 class LoginView(MethodView):
     """This class-based view handles user login and access token generation."""
