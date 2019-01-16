3e9250c5315a2297aac48dea57424fc0c314a476
Anorld Mukone <manorldsapiens@gmail.com>
2019-01-16 12:48:29 +0300

    test all incidents

--- tests/test_incident.py
+++ tests/test_incident.py
@@ -16,7 +16,7 @@ class TestFlaskApi(unittest.TestCase):
         data = json.loads(self.response.data)
         self.token = data["data"][0]["access_token"]
         self.headers=({"Authorization": "Bearer " + self.token + "_"})
-        self.header_old = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NDc1OTUwMjksImlhdCI6MTU0NzU4NzgyOSwic3ViIjoxNjEwNSwiYWRuIjoiRmFsc2UifQ.AxU19wAI4_oPw0vyTgweu7MZ4Bf4VV6tsk4pJK68GrA"
+        self.header_old = ({"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NDc1OTUwMjksImlhdCI6MTU0NzU4NzgyOSwic3ViIjoxNjEwNSwiYWRuIjoiRmFsc2UifQ.AxU19wAI4_oPw0vyTgweu7MZ4Bf4VV6tsk4pJK68GrA"+ "_"})
 
 
     def test_server_is_running(self):
@@ -269,7 +269,7 @@ class TestFlaskApi(unittest.TestCase):
         data = json.loads(response.data)
         self.assertEqual(data["status"], 401)
 
-    def test_delete_red_flag_given_id(self):
+    def test_delete_red_flag_given_id_no_token_header(self):
         incidents.append(incident3)
         id = incidents[0].id
         response = self.client.delete(
@@ -312,5 +312,82 @@ class TestFlaskApi(unittest.TestCase):
         id = incidents[0].id
         response = self.client.patch('/api/v1/red-flags/'+str(id)+'/location', data=json.dumps(new_location),
                                         content_type='application/json')
+        data = json.loads(response.data)
+        self.assertEqual(data["status"], 401)
+
+
+
+    def test_get_list_of_incidents_expired_token_header(self):
+        redflag_incident = {
+            "created_by": 5242,
+            "type": "red-flag",
+            "location": "0.112, 0.545",
+            "status": " sdfsd",
+            "images": ["sdfaf", "vfdgdf"],
+            "videos": ["video link", "fgfdgs"],
+            "comment": "This is the comment sgfd"
+        }
+        self.client.post('/api/v1/red-flags', data=json.dumps(redflag_incident),
+                         content_type='application/json', headers=self.headers)
+        response = self.client.get('/api/v1/red-flags', headers=self.header_old)
+        data = json.loads(response.data)
+        self.assertEqual(data["status"], 401)
+
+    def test_get_an_incident_expired_token_header(self):
+        id = incidents[0].id
+        response = self.client.get(
+            '/api/v1/red-flags/'+str(id), headers=self.header_old)
+        data = json.loads(response.data)
+        self.assertEqual(data["status"], 401)
+
+    def test_create_new_red_flag_expired_token_header(self):
+        response = self.client.post('/api/v1/red-flags', data=json.dumps(incident6_data_dictionary),
+                                    content_type='application/json', headers=self.header_old)
+        data = json.loads(response.data)
+        self.assertEqual(data["status"], 401)
+
+    def test_delete_red_flag_given_id_expired_token_header(self):
+        incidents.append(incident3)
+        id = incidents[0].id
+        response = self.client.delete(
+            '/api/v1/red-flags/'+str(id), headers=self.header_old)
+        data = json.loads(response.data)
+        self.assertEqual(data["status"], 401)
+
+
+    def test_update_red_flag_comment_expired_token_header(self):
+        redflag_incident = {
+            "created_by": 5242,
+            "type": "red-flag",
+            "location": "0.112, 0.545",
+            "status": " sdfsd",
+            "images": ["sdfaf", "vfdgdf"],
+            "videos": ["video link", "fgfdgs"],
+            "comment": "This is the comment sgfd"
+        }
+        self.client.post('/api/v1/red-flags', data=json.dumps(redflag_incident),
+                         content_type='application/json', headers=self.headers)
+        id = incidents[0].id
+        response = self.client.patch('/api/v1/red-flags/'+str(id)+'/comment', data=json.dumps(new_comment),
+                                     content_type='application/json', headers=self.header_old)
+        data = json.loads(response.data)
+        self.assertEqual(data["status"], 401)
+
+
+    def test_update_red_flag_location_expired_token_header(self):
+        redflag_incident = {
+                                "created_by": 5242,
+                                "type": "red-flag",
+                                "location": "0.112, 0.545",
+                                "status": " sdfsd",
+                                "images": ["sdfaf", "vfdgdf"],
+                                "videos": ["video link", "fgfdgs"],
+                                "comment": "This is the comment sgfd"
+                            }
+        self.client.post('/api/v1/red-flags', data=json.dumps(redflag_incident),
+                            content_type='application/json', headers=self.headers)
+        id = incidents[0].id
+        response = self.client.patch('/api/v1/red-flags/'+str(id)+'/location', data=json.dumps(new_location),
+                                        content_type='application/json', headers=self.header_old)
         data = json.loads(response.data)
         self.assertEqual(data["status"], 401)
