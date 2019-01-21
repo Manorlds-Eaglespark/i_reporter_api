ef124607ac46cebbc79963faad0ceb4bf67d7613
Anorld Mukone <manorldsapiens@gmail.com>
2019-01-21 16:02:09 +0300

    add create users table

--- app/databases/database.py
+++ app/databases/database.py
@@ -7,9 +7,19 @@ class Database:
     """docstring for Database class"""
 
     def __init__(self):
-        # db_name = os.getenv("DATABASE")
-        db_name = "ireporter"
         self.connection = psycopg2.connect(user="postgres", password="", host="127.0.0.1", port="5432",
-                                           database=db_name)
+                                           database="ireporter")
         self.cursor = self.connection.cursor()
         self.connection.autocommit = True
+
+    def create_all_tables(self):
+        sql_command_users_table = ("CREATE TABLE IF NOT EXISTS users"
+                                   "(id SERIAL PRIMARY KEY,"
+                                   "firstname TEXT NOT NULL,"
+                                   "lastname TEXT NOT NULL,"
+                                   "othernames TEXT NULL,"
+                                   "email TEXT NOT NULL,"
+                                   "password TEXT NOT NULL,"
+                                   "phonenumber TEXT NOT NULL,"
+                                   "username TEXT NOT NULL,"
+                                   "isadmin TEXT NOT NULL,"
